38092064d6d5ca960c3634b97edcdd04b6c4027b
bandvov <bandvov@gmail.com>
2024-05-26 17:03:32 +0300

    update queries

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -1,6 +1,7 @@
 const getAllUsersQuery = `
 SELECT id, email, first_name, last_name, created_at, phone_number, image FROM users
-WHERE deleted = FALSE;`;
+WHERE blocked = 0
+   AND deleted = 0;`;
 const getUserByIdQuery =
   "SELECT id, email, first_name, last_name, created_at, phone_number FROM users WHERE id = ?;";
 const getUserByEmailQuery =
