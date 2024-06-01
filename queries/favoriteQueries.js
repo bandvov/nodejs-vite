af47da642a66ef386d4dcece4118ddf952bdc7f3
bandvov <bandvov@gmail.com>
2024-06-01 12:09:18 +0300

    fix favorite query

--- queries/favoriteQueries.js
+++ queries/favoriteQueries.js
@@ -1,4 +1,4 @@
-const addFavoriteQuery = `INSERT INTO favorites (user_id, car_id)`;
+const addFavoriteQuery = `INSERT INTO favorites (user_id, car_id) VALUES ($1, $2)`;
 const deleteFavoriteQuery = `DELETE FROM favorites WHERE user_id = $1 AND car_id= $2`;
 
 module.exports = {
