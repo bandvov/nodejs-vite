c54a82d60700126b9bde661949e3425a0c0fd5cf
bandvov <bandvov@gmail.com>
2024-06-01 11:53:21 +0300

    fix query

--- services/carService.js
+++ services/carService.js
@@ -44,10 +44,10 @@ class CarService {
     this.db.query(query, params, callback);
   }
   addFavorite(data, callback) {
-    this.db.query(addFavoriteQuery, [data.user_id, data.car_id], callback);
+    this.db.query(addFavoriteQuery, [+data.user_id, +data.car_id], callback);
   }
   deleteFavorite(data, callback) {
-    this.db.query(deleteCarQuery, [data.user_id, data.car_id], callback);
+    this.db.query(deleteCarQuery, [+data.user_id, +data.car_id], callback);
   }
   // Add more database operations here
 }
